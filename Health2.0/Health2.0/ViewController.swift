48abf2ad414f8ff2259d5eb70d977b4ee6630310
Aleksandr Golubkin <golubkin1998@mail.ru>
2019-03-28 15:33:21 +0300

    work with scrollview

---
+++ Health2.0/Health2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health2.0
+//
+//  Created by Александр Голубкин on 28/03/2019.
+//  Copyright © 2019 Александр Голубкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
